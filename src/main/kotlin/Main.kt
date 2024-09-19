c3c0b62a4fb809ea3d010f3ce6dc744b2a75d28d
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-09-19 18:51:12 +0700

    variable

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,37 @@
 package org.zen.kotlin
 
-//this is comment
+//Data Types & Variable
 
 /*
-* this is
-* multiline comment
+* variabel:
+* var identifier: Type = initialization
 *
 * */
 
-
 fun main(){
-    println("hai")
+//    var = mutable (bisa di reassign)
+    var name: String = "Zen"
+    println("print name: " + name)
+
+    name = "Zein Irfansyah" //reassign name
+    println("print name setelah di reasign: " + name)
+
+//    val = read only
+    val perusahaan: String = "WillowLane"
+    println("print perusahaan: " + perusahaan)
+
+//    perusahaan = "WillowLame" //gak bisa di reassign, error
+//    println("print perusahaan setelah di reasign: " + perusahaan)
+
+//    operator pada variabel
+    print(name + perusahaan)
+
+    var angka1 = 1
+    var angka2 = 2
+    println(angka1 + angka2)
+
+    var angka = 1
+    angka += 2
+
+    println(angka)
 }
